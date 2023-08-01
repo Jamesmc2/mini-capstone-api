8c7e6b113167ae0a7d9413e971f49d005b7dd3bc
jimmy <jamesmc21998@gmail.com>
2023-08-01 13:04:02 -0500

    add create function via POST request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,10 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
+
+  def create
+    @product = Product.new(name: "baseball", price: 5, image_url: "https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg", description: "A white ball used for playing baseball")
+    @product.save
+    render template: "products/show"
+  end
 end
