c8a19ea7304c98c1738f89f52f091ea7eadd4f01
jimmy <jamesmc21998@gmail.com>
2023-08-01 14:14:38 -0500

    create a new route for updating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,23 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(name: "baseball", price: 5, image_url: "https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg", description: "A white ball used for playing baseball")
+    @product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_image_url],
+      description: params[:input_description],
+    )
     @product.save
     render template: "products/show"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.price = params[:price]
+    @product.name = params[:name]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render :show
   end
 end
