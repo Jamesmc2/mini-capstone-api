8aadaba5e01190851f86cfdb6158c86dd2362246
jimmy <jamesmc21998@gmail.com>
2023-08-09 11:24:47 -0500

    add order model and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
+  has_many :orders
   validates :name, uniqueness: true, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, length: { in: 10..500 }
